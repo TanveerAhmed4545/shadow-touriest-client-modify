d2db788837cbce6af84322febbed179729a98ad5
TanveerAhmed4545 <tanveer8507@gmail.com>
2026-05-04 16:23:35 +0600

    Update API endpoints to point to new backend deployment

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosPublic = axios.create({
-    baseURL: 'https://react-shadow-tourist-server.vercel.app',
+    baseURL: 'https://backendfix.vercel.app',
 })
 
 const useAxiosPublic = () => {
